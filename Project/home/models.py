8bc5001c4a54360ecb52bf998a0ab1c11ac2d31f
247724 Łyczko Łukasz <247724@edu.p.lodz.pl>
2024-03-01 02:54:48 +0100

    Added Customer model and from now html handles getting customer's information from DB

--- Project/home/models.py
+++ Project/home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=30, null=True)
+    password = models.CharField(max_length=30, null=True)
+    email = models.EmailField(null=True)
+    name = models.CharField(max_length=30, null=True)
+    surname = models.CharField(max_length=30, null=True)
+    date_of_birth = models.DateField(null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
--- Project/home/urls.py
+++ Project/home/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     #after redirecting from urls.py in Project, this is the first view that is called
     path('home/', views.home, name='home'),
-    path('profile/', views.profile, name='profile'),
+    path('profile/<str:pk>', views.profile, name='profile'),
     path('', RedirectView.as_view(url='/home/'))
 
 ]
