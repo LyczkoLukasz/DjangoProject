2e9cd9effc8c1151faf43e53b094b604cf12bef3
247724 Łyczko Łukasz <247724@edu.p.lodz.pl>
2024-03-01 01:54:53 +0100

    deleting redireciotn

--- Project/home/urls.py
+++ Project/home/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     #after redirecting from urls.py in Project, this is the first view that is called
     path('home/', views.home, name='home'),
     path('profile/', views.profile, name='profile'),
-    path('', RedirectView.as_view(url='/home/'))
 
 ]
