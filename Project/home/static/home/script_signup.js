c52ef089707188dbdfdf0861cf189f4b94edede9
WiktorGlaner <247664@edu.p.lodz.pl>
2024-03-05 04:05:18 +0100

    banner reusable template created and used in subdomains

--- Project/home/static/home/script_signup.js
+++ Project/home/static/home/script_signup.js
@@ -2,12 +2,12 @@
 document.getElementById('credentials').addEventListener('submit', function(event) {
     var password1 = document.getElementById('password1').value;
     var password2 = document.getElementById('password2').value;
-    /*
+    
     if (password1 !== password2) {
         event.preventDefault();
         document.getElementById('message').textContent = 'Passwords do not match!';
     }
     else {
         document.getElementById('message').textContent = '';
-    }*/
+    }
 });
