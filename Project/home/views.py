c706bc7fe71d2a1317f6916deb70d49b6a451059
247724 Łyczko Łukasz <247724@edu.p.lodz.pl>
2024-03-05 03:48:38 +0100

    Added protection on server side that do not pass creating user with given different passwords

--- Project/home/views.py
+++ Project/home/views.py
@@ -3,6 +3,7 @@ from .models import User
 from django.contrib import messages
 from django.contrib.auth import get_user_model, decorators, login, logout
 from django.contrib.auth.hashers import make_password
+from time import sleep
 
 
 def LoginPage(request):
@@ -47,14 +48,17 @@ def registerPage(request):
         username = request.POST.get('username')
         email = request.POST.get('email')
         password = request.POST.get('password1')
+        password2 = request.POST.get('password2')
         User = get_user_model()
+        if password != password2:
+            return redirect('register')
 
-        if User.objects.filter(email=email).exists() or User.objects.filter(username=username).exists():
-            messages.error(request, 'Email or username already exists')
-        else:
-            user = User.objects.create(username=username, email=email, password=make_password(password))
-            user.save()
-            messages.success(request, 'Account was created for ' + username)
-            return redirect('home')
+            if User.objects.filter(email=email).exists() or User.objects.filter(username=username).exists():
+                messages.error(request, 'Email or username already exists')
+            else:
+                user = User.objects.create(username=username, email=email, password=make_password(password))
+                user.save()
+                messages.success(request, 'Account was created for ' + username)
+                return redirect('home')
 
     return render(request, 'home/signup_page.html')
