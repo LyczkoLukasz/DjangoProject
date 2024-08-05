da8ca8a78c76c6b3e87e6361ed1edf5fa29909f1
247724 Łyczko Łukasz <247724@edu.p.lodz.pl>
2024-08-06 01:43:30 +0200

    Added friendship_manager app with friendship model and handling url patterns to friendship_manager app instead of home app

--- Project/friendship_manager/models.py
+++ Project/friendship_manager/models.py
@@ -9,6 +9,7 @@ class Friendship(models.Model):
     from_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='from_user')
     to_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='to_user')
     created_at = models.DateTimeField(auto_now_add=True)
+    invited = models.BooleanField(default=False)
 
     class Meta:
         unique_together = ('from_user', 'to_user')
