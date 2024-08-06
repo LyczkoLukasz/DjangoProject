7fcfbf02f8b105ee4e3739b78e60e353488a87ec
WiktorGlaner <247664@edu.p.lodz.pl>
2024-08-06 17:39:41 +0200

    friends invite system added

--- Project/friendship_manager/views.py
+++ Project/friendship_manager/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Friendship
 
 # Create your views here.
 @login_required
 def friends(request):
-    return render(request, 'friendship_manager/friends.html')
+    friends = Friendship.objects.all()
+    context = {'friends': friends}
+    return render(request, 'friendship_manager/friends.html', context)
