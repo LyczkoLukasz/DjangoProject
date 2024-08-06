fcdb331d4455d5066986c451b797c653769a62a0
247724 Łyczko Łukasz <247724@edu.p.lodz.pl>
2024-08-06 20:36:09 +0200

    Added inclusion tags instead of custom tags for handling invitations to friend list

--- Project/friendship_manager/templatetags/friendship_tags.py
+++ Project/friendship_manager/templatetags/friendship_tags.py
@@ -1,20 +1,21 @@
 
 from django import template
 from friendship_manager.models import Friendship
+from django.db.models import Q
 
 
 register = template.Library()
 
 
-@register.simple_tag
-def getListOfSendedInvitations(self, user):
-    return Friendship.objects.filter(to_user=user, is_Friend=False)
+@register.inclusion_tag('friendship_manager/templetags/listSended.html')
+def getListOfSendedInvitations(user):
+    return {'friend_requests': Friendship.objects.filter(from_user=user, is_Friend=False) }
     
-@register.simple_tag
-def getListOfReceivedInvitations(self, user):
-    return Friendship.objects.filter(from_user=user, is_Friend=False)
+@register.inclusion_tag('friendship_manager/templetags/listReceived.html')
+def getListOfReceivedInvitations(user):
+    return {'friend_requests': Friendship.objects.filter(to_user=user, is_Friend=False) }
     
-@register.simple_tag
-def getListOfFriends(self, user):
-    return Friendship.objects.filter(from_user=user, is_Friend=True).filter(to_user=user, is_Friend=True)
+@register.inclusion_tag('friendship_manager/templetags/listFriends.html')
+def getListOfFriends(user):
+    return {'friend_requests': Friendship.objects.filter( Q(from_user=user) | Q(to_user=user), is_Friend=True) }
     
