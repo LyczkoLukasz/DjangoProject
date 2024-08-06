2843e19cbe84b02de79df54ff5b84d8b1f9a6bfe
WiktorGlaner <247664@edu.p.lodz.pl>
2024-08-06 21:43:10 +0200

    friends invites and list page now fully working (probably)

--- Project/friendship_manager/templatetags/friendship_tags.py
+++ Project/friendship_manager/templatetags/friendship_tags.py
@@ -7,15 +7,15 @@ from django.db.models import Q
 register = template.Library()
 
 
-@register.inclusion_tag('friendship_manager/templetags/listSended.html')
+@register.inclusion_tag('friendship_manager/listSended.html')
 def getListOfSendedInvitations(user):
     return {'friend_requests': Friendship.objects.filter(from_user=user, is_Friend=False) }
     
-@register.inclusion_tag('friendship_manager/templetags/listReceived.html')
+@register.inclusion_tag('friendship_manager/listReceived.html')
 def getListOfReceivedInvitations(user):
     return {'friend_requests': Friendship.objects.filter(to_user=user, is_Friend=False) }
     
-@register.inclusion_tag('friendship_manager/templetags/listFriends.html')
+@register.inclusion_tag('friendship_manager/listFriends.html')
 def getListOfFriends(user):
     return {'friend_requests': Friendship.objects.filter( Q(from_user=user) | Q(to_user=user), is_Friend=True) }
     
