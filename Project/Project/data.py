12e5b3665d52e65b52f3b5880129f6f73dedaf56
WiktorGlaner <247664@edu.p.lodz.pl>
2024-08-08 11:46:40 +0200

    security2 test

---
+++ Project/Project/data.py
@@ -0,0 +1,10 @@
+DBpasswords = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'test',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '39293',
+    }
+}
