44208020871591d1bb7584ed1bab12490e6ad7b4
247724 Łyczko Łukasz <247724@edu.p.lodz.pl>
2024-08-09 19:05:45 +0200

    Beta notifications app, schema of notifcations

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -38,8 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
     'home',
     'friendship_manager',
+    'notifications',
+
 ]
 
 MIDDLEWARE = [
