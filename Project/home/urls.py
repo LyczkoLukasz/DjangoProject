22ca143d2234c4244d3c7ed0f8f9ccac8584a375
247724 Łyczko Łukasz <247724@edu.p.lodz.pl>
2024-08-26 21:46:41 +0200

    Please do not leak db password again

--- Project/home/urls.py
+++ Project/home/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('login/', views.LoginPage, name='login'),
     path('logout/', views.logoutUser, name='logout'),
     path('register/', views.registerPage, name='register'),
-    #After redirecting from urls.py in Project, this is the first view that is called
+    #after redirecting from urls.py in Project, this is the first view that is called
     path('home/', views.home, name='home'),
     path('profile/<str:pk>', views.profile, name='profile'),
     path('profile/', views.myProfile, name='myProfile'),
